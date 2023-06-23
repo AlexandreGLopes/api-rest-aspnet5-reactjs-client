8e856317c94c44db383f97d9565f1fb9caf1f515
AlexandreGLopes <alexandre_lopess@hotmail.com>
2023-06-23 02:29:14 -0300

    Adicionados gets para editar book, deletar e criar novo

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,97 +1,112 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoimage from '../../assets/logo.svg';
 
 export default function Books() {
+
+    const [books, setBooks] = useState([]);
+
+    const userName = localStorage.getItem('userName');
+    const accessToken = localStorage.getItem('accessToken');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get('api/Book/v1/asc/20/1', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            setBooks(response.data.list)
+        });
+
+        //Monitorando a accessToken abaixo. Sempre que ela mudar o useEffect vai ter que ser chamado de novo para recarregar a lista
+    }, [accessToken]);
+
+    async function deleteBook(id) {
+        try {
+            await api.delete(`api/Book/v1/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            setBooks(books.filter(book => book.id != id));
+        } catch (error) {
+            alert('Delete failed! Try again! ' + error);
+        }
+    }
+
+    async function editBook(id) {
+        try {
+            navigate(`/book/new/${id}`)
+        } catch (error) {
+            alert('Edit Book failed! Try again! ' + error);
+        }
+    }
+
+    async function logout() {
+        try {
+            await api.get('api/auth/v1/revoke', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            localStorage.clear();
+            navigate('/');
+        } catch (error) {
+            alert('Logout failed! ' + error);
+        }
+    }
+
     return (
         <div className='book-container'>
             <header>
                 <img src={logoimage} alt='logo Alexandre' />
-                <span>Welcome, <strong>Alexandre</strong></span>
-                <Link className='button' to="/book/new">Add New Book</Link>
-                <button type='button'>
+                <span>Welcome, <strong>{userName.toUpperCase()}</strong></span>
+                <Link className='button' to="/book/new/0">Add New Book</Link>
+                {/* No onClick do button abaixo não precisa de Arrow Function para evitar loop.
+                Porque não tem parâmetro na chamada da função */}
+                <button onClick={logout} type='button'>
                     <FiPower size={18} color='#251FC5' />
                 </button>
             </header>
 
             <h1>Registered Books</h1>
             <ul>
-            <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5' />
-                    </button>
-
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5' />
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5' />
-                    </button>
-
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5' />
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5' />
-                    </button>
-
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5' />
-                    </button>
-                </li>
-                <li>
-                    <strong>Title:</strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author:</strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price:</strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release date:</strong>
-                    <p>12/07/2017</p>
-
-                    <button type='button'>
-                        <FiEdit size={20} color='#251FC5' />
-                    </button>
-
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251FC5' />
-                    </button>
-                </li>
+                {books.map(book => (
+                    <li key={book.id}>
+                        <strong>Title:</strong>
+                        <p>{book.title}</p>
+                        <strong>Author:</strong>
+                        <p>{book.author}</p>
+                        <strong>Price:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
+                        <strong>Release date:</strong>
+                        <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
+    
+                        {/* No onClick do botão não podemos chamar a função sem a Arrow Function. 
+                        Se fizermos isso, a função será chamada em loop e assim carregar várias telas de books.
+                        A Arrow Function previne esse comportamento */}
+                        <button onClick={() => editBook(book.id)} type='button'>
+                            <FiEdit size={20} color='#251FC5' />
+                        </button>
+    
+                        {/* No onClick do botão não podemos chamar a função sem a Arrow Function. 
+                        Se fizermos isso, a função será chamada em loop assim que carregar a tela e a base toda será excluída.
+                        A Arrow Function previne esse comportamento */}
+                        <button onClick={() => deleteBook(book.id)} type='button'>
+                            <FiTrash2 size={20} color='#251FC5' />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
--- src/pages/NewBook/index.js
+++ src/pages/NewBook/index.js
@@ -1,28 +1,107 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css'
 
 import logoimage from '../../assets/logo.svg';
 
 export default function NewBook() {
+
+    const [id, setId] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [title, setTitle] = useState('');
+    const [launchDate, setLaunchDate] = useState('');
+    const [price, setPrice] = useState('');
+
+    const { bookId } = useParams();
+
+    const navigate = useNavigate();
+
+    const accessToken = localStorage.getItem('accessToken');
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
+    useEffect(() => {
+        if (bookId === '0') return;
+        else loadBook();
+
+        // Sempre que tiver mudança no bookId vai ter que recarregar a pagina
+    }, bookId);
+
+    async function loadBook() {
+        try {
+            const response = await api.get(`api/book/v1/${bookId}`, authorization);
+
+            let adjustedDate = response.data.launchDate.split("T", 10)[0];
+
+            setId(response.data.id);
+            setTitle(response.data.title);
+            setAuthor(response.data.author);
+            setLaunchDate(adjustedDate);
+            setPrice(response.data.price);
+        } catch (error) {
+            alert('Error recovering Book! ' + error);
+            navigate('/books');
+        }
+    }
+
+    async function createNewBook(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            author,
+            launchDate,
+            price,
+        }
+
+        try {
+            await api.post('api/Book/v1', data, authorization);
+        } catch (error) {
+            alert('Error while recording Book! Try again. ' + error)
+        }
+        navigate('/books');
+    }
+
     return (
         <div className="new-book-container">
             <div className="content">
                 <section className="form">
                     <img src={logoimage} alt='Alexandre logo' />
                     <h1>Add New Book</h1>
-                    <p>Enter the book information and click on 'Add'!</p>
+                    <p>Enter the book information and click on 'Add'! #${bookId}</p>
                     <Link className="back-link" to="/books">
                         <FiArrowLeft size={16} color='#251fc5' />
                     </Link>
                 </section>
-                <form>
-                    <input placeholder='Title' />
-                    <input placeholder='Author' />
-                    <input type='date' />
-                    <input placeholder='Price' />
+                <form onSubmit={createNewBook}>
+                    <input
+                        placeholder='Title' 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <input
+                        placeholder='Author'
+                        value={author}
+                        onChange={e => setAuthor(e.target.value)}
+                     />
+                    <input
+                        type='date' 
+                        value={launchDate}
+                        onChange={e => setLaunchDate(e.target.value)}
+                    />
+                    <input
+                        placeholder='Price' 
+                        value={price}
+                        onChange={e => setPrice(e.target.value)}
+                    />
 
                     <button className="button" type='submit'>Add</button>
                 </form>
