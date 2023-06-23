56f97b1df198af7639279cd4a301b2eea808fcd3
AlexandreGLopes <alexandre_lopess@hotmail.com>
2023-06-23 00:31:03 -0300

    Integrando Login ao consumo da API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import api from '../../services/api'
+
 import './styles.css';
 
 import logoimage from '../../assets/logo.svg'
 import padlock from '../../assets/padlock.png'
 
 export default function Login() {
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    // para garantir que a função seja executada com sucesso vamos torná-la assícrona. Porque ele pode passar direto pela chamada a api.
+    async function login(e) {
+        // e.preventDefault() prevenir o refresh da pagina na ação de submit
+        e.preventDefault();
+
+        const data = {
+            userName,
+            password,
+        };
+
+        try {
+            // para garantir que a função seja executada com sucesso vamos torná-la assícrona. Porque ele pode passar direto pela chamada a api.
+            const response = await api.post('api/auth/v1/signin', data);
+
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+
+            navigate('/books');
+        } catch (error) {
+            alert('Login failed! Try again! ' + error);
+        }
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoimage} alt="Alexandre Logo"/>
-                <form>
+                <form onSubmit={login}>
                     <h1>Access your account</h1>
-                    <input placeholder='Username' />
-                    <input type='password' placeholder='Password' />
+
+                    <input 
+                        placeholder='Username'
+                        value={userName}
+                        onChange={e => setUserName(e.target.value)}
+                    />
+
+                    <input 
+                        type='password' 
+                        placeholder='Password' 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
                     <button className='button' type='submit'>Login</button>
                 </form>
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Books from './pages/Books';
+import NewBook from './pages/NewBook';
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" exact element={<Login />} />
+                <Route path="/books" element={<Books />} />
+                <Route path="/book/new" element={<NewBook />} />
             </Routes>
         </BrowserRouter>
     );
